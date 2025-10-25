e75e1b166df562ee9d5de6eabcb96dfcf016f2c8
Perchi <perchiban@pm.me>
2025-10-25 12:00:48 -0300

    keybind first

---
+++ src/main/java/net/papasconcheddar/cloudbackupmod/event/ModClientEvents.java
@@ -0,0 +1,31 @@
+package net.papasconcheddar.cloudbackupmod.event;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.InputEvent;
+import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
+import net.papasconcheddar.cloudbackupmod.CloudBackupMod;
+import net.papasconcheddar.cloudbackupmod.util.KeyBinding;
+
+public class ModClientEvents {
+    @EventBusSubscriber(modid = CloudBackupMod.MODID, value = Dist.CLIENT)
+    public static class ClientForgeEvents {
+        @SubscribeEvent
+        public static void onKeyInput(InputEvent.Key event) {
+            if(KeyBinding.BACKUP_MENU_KEY.consumeClick()) {
+                Minecraft.getInstance().player.sendSystemMessage(Component.literal("Pressed a Key!"));
+            }
+        }
+    }
+
+    @EventBusSubscriber(modid = CloudBackupMod.MODID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
+    public static class ClientModBusEvents {
+        @SubscribeEvent
+        public static void onKeyRegister(RegisterKeyMappingsEvent event) {
+            event.register(KeyBinding.BACKUP_MENU_KEY);
+        }
+    }
+}
---
+++ src/main/java/net/papasconcheddar/cloudbackupmod/util/KeyBinding.java
@@ -0,0 +1,14 @@
+package net.papasconcheddar.cloudbackupmod.util;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
+import net.neoforged.neoforge.client.settings.KeyConflictContext;
+import org.lwjgl.glfw.GLFW;
+
+public class KeyBinding {
+    public static final String KEY_CATEGORY_CLOUDBACKUPMOD = "key.category.cloudbackupmod.cloudbackup";
+    public static final String KEY_BACKUP_MENU = "key.tutorialmod.backup_menu";
+
+    public static final KeyMapping BACKUP_MENU_KEY = new KeyMapping(KEY_BACKUP_MENU, KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_Z, KEY_CATEGORY_CLOUDBACKUPMOD);
+}
