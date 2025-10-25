0624030b849ee5d1e83488505581fd696eb7374e
mirandapabloagustin <mirandapabloagustin@gmail.com>
2025-10-25 13:13:21 -0300

    Te la Empujo ahh

--- src/main/java/net/papasconcheddar/cloudbackupmod/screen/custom/PanelScreen.java
+++ src/main/java/net/papasconcheddar/cloudbackupmod/screen/custom/PanelScreen.java
@@ -3,12 +3,26 @@ package net.papasconcheddar.cloudbackupmod.screen.custom;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 import net.minecraft.network.chat.Component;
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.papasconcheddar.cloudbackupmod.CloudBackupMod;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Inventory;
 
-public class PanelScreen {
+public class PanelScreen extends AbstractContainerScreen<PanelMenu>{
+
+    private static final ResourceLocation BG = new ResourceLocation("cloudbackupid", "textures/menu.png");
+
 
     public Component Panelcreen(){
         return Component.literal("PanelScreen");
     }
+
+
+
 
 
 
